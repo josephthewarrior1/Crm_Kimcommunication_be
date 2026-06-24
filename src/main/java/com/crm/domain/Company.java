5b03a3cbd5483703293aa365ac18437fd07c1829
joseph <josephsetiawan71@gmail.com>
2026-06-24 14:26:03 +0700

    fix: resolve jackson serialization bytebuddy proxy error by adding JsonIgnoreProperties

--- src/main/java/com/crm/domain/Company.java
+++ src/main/java/com/crm/domain/Company.java
@@ -1,5 +1,6 @@
 package com.crm.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/com/crm/domain/Event.java
+++ src/main/java/com/crm/domain/Event.java
@@ -1,5 +1,6 @@
 package com.crm.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
